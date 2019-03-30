b4c715f3bfd2a2bbcbefd74aec86a4ccc2632fd7
pawel.basiak01@gmail.com <pawel.basiak01@gmail.com>
2019-03-30 13:27:51 +0100

    Heading component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+function Heading(props) {
+  return (
+    <h1>
+      {props.title}
+    </h1>
+  );
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        React by devpebe
+        <Heading title="React App"/>
       </div>
     );
   }
